2692d8531c1734e81526416d79a5ae571d187b19
Débora Fernandes C <95572299+Deboraaluna@users.noreply.github.com>
2022-08-22 19:14:06 -0300

    metodo equals

---
+++ metodo equals/exemplo2.java
@@ -0,0 +1,22 @@
+enum Etacao {
+VERAO,
+OUTONO,
+IVERNO,
+PRIMAVERA;
+}
+
+class Main {
+public static void main(String[] args) {
+String estacao = "PRIMAVEIRA";
+
+if(estacao.equals(Estacao.PRIMAVERA)){
+System.out.println("Os valores sao iguais");
+}
+else{
+System.out.println("Os valores são diferentes.");
+}
+}
+}
+
+
+Os Valores são diferentes
